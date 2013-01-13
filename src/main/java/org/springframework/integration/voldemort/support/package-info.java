068f388d5ffe05e360e4b7641eec0d7a7b84eabe
Lukasz Antoniak <Lukasz.Antoniak@gmail.com>
2013-01-13 06:26:48 -0800

    Cleanup

--- src/main/java/org/springframework/integration/voldemort/support/package-info.java
+++ src/main/java/org/springframework/integration/voldemort/support/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Provides various support classes used across Spring Integration Voldemort Components.
+ * Provides various support classes used across Spring Integration Voldemort components.
  */
 package org.springframework.integration.voldemort.support;
